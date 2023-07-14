7e23b06368f14aab9bc8208c2a57aa24a9a6cf5e
loeasy68 <90572477+loeasy68@users.noreply.github.com>
2023-07-14 16:00:46 +0000

    added things

--- react-mini/src/App.js
+++ react-mini/src/App.js
@@ -1,7 +1,10 @@
 
 function App() {
   return (
-    <h1>Test</h1>
+    <>
+        <h1>Welcome to react mini</h1>
+        <p>React mini takes out all of the stuff that is unnessary in create-react-app install it using npm with the following command</p>
+    </>
   );
 }
 
